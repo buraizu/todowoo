58697131ddb601900b9acac906e81e81c99af6d0
Bryce Eadie <bryce.eadie@gmail.com>
2021-08-02 18:10:49 -0700

    Adds functionality for listing todos by user

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -54,4 +55,5 @@ def createtodo(request):
             return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':'Invalid values were provided to one or more fields. Please check you entries and try again.'})
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos':todos})
